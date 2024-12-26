61ec02f481b4fd0a8d5ddb6e305b88d910cfc997
unknown <sojibjubayer@gmail.com>
2024-12-27 00:39:46 +0600

    route solved

---
+++ src/app/see-transaction/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SeeTransactionList = () => {
+    return (
+        <div className='h-screen'>
+            See Transaction as List
+        </div>
+    );
+};
+
+export default SeeTransactionList;
