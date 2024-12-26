2ec0ab679cdd9928966299e2fac11afe2251a7d5
unknown <sojibjubayer@gmail.com>
2024-12-27 00:58:00 +0600

    cors setup

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -24,7 +24,7 @@ const SignUp = () => {
     };
 
     try {
-      const resp = await fetch(`http://localhost:3000/signup/api`, {
+      const resp = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/signup/api`, {
         method: "POST",
         body: JSON.stringify(newUser),
         headers: {
